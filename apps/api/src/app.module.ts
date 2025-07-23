428874f671a73b5f418589b0425e491fb133ae72
Muhamad Amir <m.amirazmi@hotmail.com>
2025-07-23 22:43:24 +0800

    Add swagger

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from './common/common.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 
-console.log('process', process.env);
-
 @Module({
   imports: [
     ConfigModule.forRoot({
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('RMS API')
+    .setDescription('The API documentation for Repair Management System RMS')
+    .setVersion('1.0.0')
+    .addServer(configService.get<string>('HOST') + '/api')
+    .build();
+  const swaggerDocumentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, swaggerDocumentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   const port = configService.get<number>('PORT') ?? 9900;
   await app.listen(port, () => {
     console.log('Listening at port:', port);
