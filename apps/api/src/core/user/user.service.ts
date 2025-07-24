97e1129dc16bc7fc9f85b45c784301d0852d77d0
m-amirazmi <m.amirazmi@hotmail.com>
2025-07-24 17:20:13 +0800

    Add auth controller

--- apps/api/src/core/user/user.service.ts
+++ apps/api/src/core/user/user.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { DATABASE_CONNECTION } from 'src/common/database/database-connection';
+import * as schema from './_schema';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @Inject(DATABASE_CONNECTION)
+    private readonly db: NodePgDatabase<typeof schema>,
+  ) {}
+
+  // For platform owners & tenant owners
+  async getUserByEmail(email: string) {
+    return this.db
+      .select()
+      .from(schema.users)
+      .where(eq(schema.users.email, email))
+      .limit(1)
+      .then((rows) => rows[0]);
+  }
+}
